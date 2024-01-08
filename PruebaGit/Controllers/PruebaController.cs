8594841b938bed75b1101a33d2cf88a5fb0c8b21
JoseBsp <jose.sanchez.bsp@gmail.com>
2024-01-08 14:11:22 -0600

    Prueba rama2

---
+++ PruebaGit/Controllers/PruebaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaGit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+    }
+}
